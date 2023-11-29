bf8e3c471da835dbd8d325291c1dff5e1a579056
dhipgraby <keneth.zambrano4@gmail.com>
2023-11-29 14:42:59 +0100

    APIs with https

--- src/helpers/env_variables.ts
+++ src/helpers/env_variables.ts
@@ -1,7 +1,7 @@
 //PROD
-export const AUTH_API = "http://185.189.27.38:3001/"
-export const USER_API = "http://185.189.27.38:3002/"
-export const ASTEROID_API = "http://185.189.27.38:3003/"
+export const AUTH_API = "https://185.189.27.38:3001/"
+export const USER_API = "https://185.189.27.38:3002/"
+export const ASTEROID_API = "https://185.189.27.38:3003/"
 
 //LOCALHOST
 // export const AUTH_API = "http://localhost:3001/"
