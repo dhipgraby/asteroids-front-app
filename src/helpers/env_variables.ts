776268a1647783a7fcb557bbc906561c2aafe041
dhipgraby <keneth.zambrano4@gmail.com>
2023-11-29 14:05:10 +0100

    fix prod api variables

--- src/helpers/env_variables.ts
+++ src/helpers/env_variables.ts
@@ -1,3 +1,11 @@
-export const AUTH_API = "http://localhost:3001/"
-export const USER_API = "http://localhost:3002/"
-export const ASTEROID_API = "http://localhost:3003/"
+//PROD
+export const AUTH_API = "http://185.189.27.38:3001/"
+export const USER_API = "http://185.189.27.38:3002/"
+export const ASTEROID_API = "http://185.189.27.38:3003/"
+
+//LOCALHOST
+// export const AUTH_API = "http://localhost:3001/"
+// export const USER_API = "http://localhost:3002/"
+// export const ASTEROID_API = "http://localhost:3003/"
+
+
